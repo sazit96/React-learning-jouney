b1593472b93f3d0d57f2ac958dc2714ef71a7cc1
Ehosanul Islam Sazit <sazitislam96@gmail.com>
2024-11-13 11:09:45 +0600

    tody i build another mini react project  name React Facts

---
+++ 02.React Facts Projact/React Facts/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Components/navbar';
+import Main from './Components/main';
+
+export default function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
